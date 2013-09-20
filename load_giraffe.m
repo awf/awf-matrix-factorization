c7656056bdfea305e4062d8be8cc02ba1e110aa7
awf <awf@fitzgibbon.ie>
2013-09-20 17:53:30 +0100

    add small option

--- load_giraffe.m
+++ load_giraffe.m
@@ -1,10 +1,13 @@
-function [M,W] = load_giraffe
+function [M,W] = load_giraffe(small)
 % LOAD_GIRAFFE Load giraffe data
 %              [M,W] = load_giraffe
-
 
 M = load('data/giraffe.M.txt');
 M = M(:,2:end);
 W = isfinite(M);
 M(~W) = 0;
 
+if nargin >= 1 && any(small)
+    M=M(1:40,1:40);
+    W=W(1:40,1:40);
+end
